7e61069b1f2ff08c84fb2e048bab1729c4749d24
hans <hans.mou@feibit.com>
2023-12-27 10:07:43 +0800

    clear up

--- code/han.js
+++ code/han.js
@@ -1,19 +1,14 @@
-
-new
-```js
-function Person (name) {
-  this.name = name
-}
+new (function Person(name) {
+  this.name = name;
+})();
 Person.prototype.eat = function () {
-  console.log('Eatting')
-}
-var lindaidai = new Person('LinDaiDai')
-console.log(lindaidai)
-lindaidai.eat()
-```
+  console.log("Eatting");
+};
+var lindaidai = new Person("LinDaiDai");
+console.log(lindaidai);
+lindaidai.eat();
 
-```js
-function create () {
+function create() {
   // 1. 获取构造函数，并且删除 arguments 中的第一项
   var Con = [].shift.call(arguments);
   // 2. 创建一个空的对象并链接到构造函数的原型，使它能访问原型中的属性
@@ -23,5 +18,3 @@ function create () {
   // 4. 优先返回构造函数返回的对象
   return ret instanceof Object ? ret : obj;
 }
-
-```
